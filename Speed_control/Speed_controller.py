bf0170929340df1493dfab80d6c5b6600ad4a22f
Parcius <vilee@yandex.ru>
2024-04-07 13:16:57 +0300

    Создание станка

---
+++ Speed_control/Speed_controller.py
@@ -0,0 +1,4 @@
+
+X_axis_speed = 3000 m/min
+Y_axis_speed = 4000 m/min
+Z_axis_speed = 2500 m/min
---
+++ milling_machine.py
@@ -0,0 +1,4 @@
+Tool_rotor_speed = 12000 rpm
+X_stroke = 2000 mm
+Y_stroke = 1000 mm
+Z_stroke =  800 mm 
